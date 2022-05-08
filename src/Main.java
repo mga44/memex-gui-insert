b88bbe1aa2187b3d817dcfee294fa490c04b10f4
mga44 <39274754+mga44@users.noreply.github.com>
2022-05-08 20:30:53 +0200

    running main as window, gui v .01

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,164 @@
-import java.io.FileInputStream;
-import java.util.Properties;
 
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JEditorPane;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
 
-import mga44.gui.MemexInsertionWindow;
+import logic.ndtl.ContentType;
+import mga44.gui.action.SaveEntityAction;
 
-public class Main {
+public class Main extends JFrame {
+	private static final long serialVersionUID = 5373416828900017783L;
 
-	private static Properties props;
+	private JPanel contentPane;
+	private JTextField textField;
 
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
 		initialize();
-		MemexInsertionWindow window = new MemexInsertionWindow();
-		window.run();
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 	private static void initialize() {
 		try {
-			props = new Properties();
-			props.load(new FileInputStream("./resources/app.properties"));
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setTitle("Memex");
+		setFont(new Font("Dialog", Font.PLAIN, 14));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[] { 0, 0, 0, 0, 0, 0 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0, 0, 0, 0, 0 };
+		gbl_contentPane.columnWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 1.0, 1.0, 0.0, Double.MIN_VALUE };
+		contentPane.setLayout(gbl_contentPane);
+
+		JLabel lblNewLabel = new JLabel("Title");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel.gridx = 0;
+		gbc_lblNewLabel.gridy = 0;
+		contentPane.add(lblNewLabel, gbc_lblNewLabel);
+
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.gridwidth = 4;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 0;
+		contentPane.add(textField, gbc_textField);
+		textField.setColumns(10);
+
+		JLabel lblNewLabel_1 = new JLabel("Type");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 1;
+		contentPane.add(lblNewLabel_1, gbc_lblNewLabel_1);
+
+		JComboBox<ContentType> comboBox = new JComboBox<>();
+		comboBox.setModel(new DefaultComboBoxModel<>(ContentType.values()));
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.gridwidth = 4;
+		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 1;
+		gbc_comboBox.gridy = 1;
+		contentPane.add(comboBox, gbc_comboBox);
+
+		JLabel lblNewLabel_2 = new JLabel("Note");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 0;
+		gbc_lblNewLabel_2.gridy = 2;
+		contentPane.add(lblNewLabel_2, gbc_lblNewLabel_2);
+
+		JEditorPane editorPane = new JEditorPane();
+		editorPane.setToolTipText("");
+		GridBagConstraints gbc_editorPane = new GridBagConstraints();
+		gbc_editorPane.gridwidth = 4;
+		gbc_editorPane.insets = new Insets(0, 0, 5, 5);
+		gbc_editorPane.fill = GridBagConstraints.BOTH;
+		gbc_editorPane.gridx = 1;
+		gbc_editorPane.gridy = 2;
+		contentPane.add(editorPane, gbc_editorPane);
+
+		JLabel lblNewLabel_3 = new JLabel("Quote");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 0;
+		gbc_lblNewLabel_3.gridy = 3;
+		contentPane.add(lblNewLabel_3, gbc_lblNewLabel_3);
+
+		JEditorPane editorPane_1 = new JEditorPane();
+		GridBagConstraints gbc_editorPane_1 = new GridBagConstraints();
+		gbc_editorPane_1.gridwidth = 4;
+		gbc_editorPane_1.insets = new Insets(0, 0, 5, 5);
+		gbc_editorPane_1.fill = GridBagConstraints.BOTH;
+		gbc_editorPane_1.gridx = 1;
+		gbc_editorPane_1.gridy = 3;
+		contentPane.add(editorPane_1, gbc_editorPane_1);
+
+		JButton btnNewButton = new JButton("Save");
+		btnNewButton.setIcon(new ImageIcon("resources/icon/download.jpg"));
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				SaveEntityAction action = new SaveEntityAction();
+				action.run();
+			}
+		});
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.gridx = 4;
+		gbc_btnNewButton.gridy = 4;
+		contentPane.add(btnNewButton, gbc_btnNewButton);
+	}
+
 }

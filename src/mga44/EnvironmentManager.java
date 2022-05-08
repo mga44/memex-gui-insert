9c20619e0681797f9ffa74fab72a109f9c7f29bb
mga44 <39274754+mga44@users.noreply.github.com>
2022-05-08 21:51:17 +0200

    Update EnvironmentManager.java

--- src/mga44/EnvironmentManager.java
+++ src/mga44/EnvironmentManager.java
@@ -1,9 +1,36 @@
 package mga44;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class EnvironmentManager {
+	// TODO better name, structure
+	private Path memexDirectory;
+	private Path databaseFile;
 
-	public static final Path memexDirectory = Path.of(".");
-	public static final Path databaseFile = Path.of(memexDirectory.toString(), "content", "data.ndtl");
+	private static EnvironmentManager current;
+
+	private EnvironmentManager() {
+	}
+
+	public static EnvironmentManager getInstance() {
+		if (current != null)
+			return current;
+
+		current = new EnvironmentManager();
+		String property = System.getProperty("memex_directory"); // TODO to constant
+		current.memexDirectory = Paths.get(property);
+		current.databaseFile = Path.of(current.memexDirectory.toString(), "content", "data.ndtl");
+
+		return current;
+	}
+
+	public Path getMemexDirectory() {
+		return memexDirectory;
+	}
+
+	public Path getDatabaseFile() {
+		return databaseFile;
+	}
+
 }

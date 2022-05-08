de286bcc68913ac9ad83d1815507e9dca4663c52
mga44 <39274754+mga44@users.noreply.github.com>
2022-05-08 20:27:08 +0200

    saving content action

--- src/mga44/gui/action/SaveEntityAction.java
+++ src/mga44/gui/action/SaveEntityAction.java
@@ -1,25 +1,36 @@
 package mga44.gui.action;
 
-import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
 import java.util.List;
 
 import logic.ndtl.ContentType;
+import mga44.EnvironmentManager;
 import mga44.io.ndtl.MemexEntity;
 import mga44.io.ndtl.MemexEntity.MemexEntityBuilder;
 
 public class SaveEntityAction {
-
-	{
+	public void run() {
 		ContentType type = null;
-		String title = null;
-		File attachment = null;
-		List<String> tags = null;
-		String note = null;
-		String quote = null;
+		String title = "";
+		Path attachment = Path.of("");
+		List<String> tags = Collections.emptyList();
+		String note = "";
+		String quote = "";
 
 		MemexEntityBuilder builder = MemexEntity.builder().withType(type).withTitle(title);
-		if (attachment != null)
-			builder.withAttachment(attachment);
+		if (attachment != null) {
+			Path destination = Path.of(EnvironmentManager.memexDirectory.toString(), "content", "media",
+					attachment.getName(attachment.getNameCount() - 1).toString());
+			try {
+				Files.copy(attachment, destination);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} // TODO test same file names
+			builder.withAttachment(destination);
+		}
 
 		if (note != null)
 			builder.withNote(note);
@@ -27,6 +38,14 @@ public class SaveEntityAction {
 		if (quote != null)
 			builder.withQuote(quote);
 
+		if (!tags.isEmpty())
+			builder.withTags(tags);
+
 		MemexEntity entity = builder.build();
+		try {
+			entity.saveTo(EnvironmentManager.databaseFile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }

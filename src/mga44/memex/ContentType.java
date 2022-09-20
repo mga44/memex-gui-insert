a6d2ffa90f3557c6e4a470d3b4e9e4f9513ac340
mga44 <39274754+mga44@users.noreply.github.com>
2022-09-20 17:47:40 +0200

    ref: formatting

--- src/mga44/memex/ContentType.java
+++ src/mga44/memex/ContentType.java
@@ -1,19 +1,29 @@
 package mga44.memex;
 
-public enum ContentType
-{
-	TOOL("Tool"), PODCAST("Podcast"), LIST("List"), MUSIC("Music"), ARTICLE("Article"), BOOK("Book"), VIDEO("Video"),
-	LECTURE("Lecture"), IMAGE("Image"), QUOTE("Quote"), NOTE("Note"), TERM("Term"), GAME("Game"),
-	ENCYCLOPEDIA("Encyclopedia");
+public enum ContentType {
+    TOOL("Tool"),
+    PODCAST("Podcast"),
+    LIST("List"),
+    MUSIC("Music"),
+    ARTICLE("Article"),
+    BOOK("Book"),
+    VIDEO("Video"),
+    LECTURE("Lecture"),
+    IMAGE("Image"),
+    QUOTE("Quote"),
+    NOTE("Note"),
+    TERM("Term"),
+    GAME("Game"),
+    ENCYCLOPEDIA("Encyclopedia");
 
-	private String name;
+    private final String name;
 
-	ContentType(String name) {
-		this.name = name;
-	}
+    ContentType(String name) {
+        this.name = name;
+    }
 
-	@Override
-	public String toString() {
-		return name;
-	}
+    @Override
+    public String toString() {
+        return name;
+    }
 }

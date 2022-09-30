d5a7f6cdbcdf7695ac14754a1b0076f6f2b3f3fd
mga44 <39274754+mga44@users.noreply.github.com>
2022-09-30 18:20:56 +0200

    ref: extraction save action to separate class

---
+++ src/mga44/memex/SaveButtonAction.java
@@ -0,0 +1,37 @@
+package mga44.memex;
+
+import mga44.gui.MainWindow;
+import mga44.gui.action.SaveEntityAction;
+import mga44.io.ndtl.MemexEntity;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.time.ZonedDateTime;
+
+public class SaveButtonAction extends MouseAdapter {
+
+    private final MainWindow window;
+
+    public SaveButtonAction(MainWindow window) {
+        this.window = window;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        //@formatter:off
+        MemexEntity.MemexEntityBuilder builder = new MemexEntity.MemexEntityBuilder(window.title.getText())
+                .withType(((ContentType) window.type.getSelectedItem()))
+                .withNote(window.note.getText())
+                .withQuote(window.quote.getText())
+                .withLink(window.link.getText())
+                .withAttachment(window.file.getText())
+                .withTags(window.tags.getText())
+                .withCreated(ZonedDateTime.now());
+        //@formatter:on
+        SaveEntityAction action = new SaveEntityAction(builder.build());
+        action.run();
+
+        System.exit(0);
+
+    }
+}
